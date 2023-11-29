61b885a0f3087be6125c07378f158c2dce134826
SvenSven <mikorecare@gmail.com>
2023-11-30 02:42:40 +0800

    FEAT: imported FormsModule for ngModel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ZyllemApiService } from './app.service';
 import { ArticleModule } from './components/articles';
@@ -12,6 +12,7 @@ import { ArticleModule } from './components/articles';
 
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     ArticleModule
   ],
