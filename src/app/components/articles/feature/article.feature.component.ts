407db11f96ffb2481245ae2040b139e56a41e56d
SvenSven <mikorecare@gmail.com>
2023-11-29 16:31:28 +0800

    FEAT: Renamed components from FeatureComponent to ArticleFeatureComponent

---
+++ src/app/components/articles/feature/article.feature.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-feature',
+  templateUrl: './article.feature.component.html',
+  styleUrls: ['./article.feature.component.css']
+})
+export class ArticleFeatureComponent {
+
+}
