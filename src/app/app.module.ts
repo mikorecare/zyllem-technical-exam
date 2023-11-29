684e6d1e3706152f8b0152ead33b5875d491af8c
SvenSven <mikorecare@gmail.com>
2023-11-30 04:04:28 +0800

    FEAT: added view-details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ZyllemApiService } from './app.service';
 import { ArticleModule } from './components/articles';
+import { ViewDetailsComponent } from './components/view-details/view-details.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    ViewDetailsComponent,
 
   ],
   imports: [
