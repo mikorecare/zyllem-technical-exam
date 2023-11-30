ddbfaadcca53a2350623c21a78b72f0d5a465868
SvenSven <mikorecare@gmail.com>
2023-11-30 23:23:02 +0800

    FEAT: Applied _id

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -31,18 +31,7 @@ export class HomeComponent implements OnInit {
       }
     
       ngOnInit(): void {
-        this.api.get('/articles')
-        .then((result)=>{
-            console.log("result")
-            if(result){
-                this.videoArticleHighlights(result)  
-              }
-              console.log("video article highlight",this.videoArticleHighlight);
-              console.log("final article array",this.results);
-        })
-        .catch((err)=>{
-            console.warn(err)
-        })
+        this.getArticles()
         // this.apiService.getArticles()
         //   .subscribe(result => {
     
@@ -55,12 +44,28 @@ export class HomeComponent implements OnInit {
         //     console.log("final article array",this.results);
         //   });
       }
+
+       async getArticles(){
+           await this.api.get('/articles')
+            .then((result)=>{
+                console.log("RESULT:    ",result)
+                if(result){
+                    this.videoArticleHighlights(result)  
+                  }
+                  console.log("video article highlight",this.videoArticleHighlight);
+                  console.log("final article array",this.results);
+            })
+            .catch((err)=>{
+                console.warn(err)
+            })
+        }
     
        videoArticleHighlights(results:Article[]):void{
+        console.log("Video Article Highlights: <BEFORE>", results)
         let videoArticle : VideoArticle[]= results.filter(res =>res.type === "VIDEO") as VideoArticle [];
         this.videoArticleHighlight = videoArticle.sort((a,b)=> new Date(b.publishedAt).getTime()- new Date(a.publishedAt).getTime())[0];
         
-        let index = results.findIndex(article=> article.id === this.videoArticleHighlight.id);
+        let index = results.findIndex(article=> article._id === this.videoArticleHighlight._id);
         if (index !== -1) {
           
           results.splice(index, 1);
@@ -77,6 +82,7 @@ export class HomeComponent implements OnInit {
       }
     
       articleFilter(): void {
+        console.log("Filter Triggered")
         if (this.filter === 'All') {
           this.filteredResults = this.results;
         } else {
