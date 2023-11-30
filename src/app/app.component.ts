18bbbc14f33efc12d131df72b6f79560c250d67b
SvenSven <mikorecare@gmail.com>
2023-11-30 14:12:22 +0800

    FEAT: Transfer app-root to HomeComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,77 +1,12 @@
-import {
-  ChangeDetectionStrategy, ChangeDetectorRef, Component,
-  OnInit
+import {Component,
 } from '@angular/core';
 
-import { ZyllemApiService } from "./app.service";
-import { Article, ArticleType, VideoArticle } from './model/article';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 
-  constructor(
-    private readonly apiService: ZyllemApiService,
-    private readonly cdr: ChangeDetectorRef
-  ) { }
-
-  private results!: Article[];
-  private filteredResults!:Article[];
-  filter:string = 'All';
-  videoArticleHighlight!: VideoArticle;
-  test:any = []
-
-  get articles() {
-    return this.filteredResults;
-  }
-
-  ngOnInit(): void {
-    this.apiService.getArticles()
-      .subscribe(result => {
-
-        if(result){
-          this.videoArticleHighlights(result)
-          
-        }
-        this.cdr.markForCheck()
-        console.log("video article highlight",this.videoArticleHighlight);
-        console.log("final article array",this.results);
-      });
-  }
-
-   videoArticleHighlights(results:Article[]):void{
-    let videoArticle : VideoArticle[]= results.filter(res =>res.type === "VIDEO") as VideoArticle [];
-    this.videoArticleHighlight = videoArticle.sort((a,b)=> new Date(b.publishedAt).getTime()- new Date(a.publishedAt).getTime())[0];
-    
-    let index = results.findIndex(article=> article.id === this.videoArticleHighlight.id);
-    if (index !== -1) {
-      
-      results.splice(index, 1);
-    }
-    
-    this.results = results;
-
-
-    this.articleFilter();
-  }
-
-  logMe(){
-    console.log(this.filter);
-  }
-
-  articleFilter(): void {
-    if (this.filter === 'All') {
-      this.filteredResults = this.results;
-    } else {
-      const filteredType: ArticleType = this.filter.toUpperCase() as ArticleType;
-      this.filteredResults = this.results.filter(res => res.type === filteredType);
-    }
-  
-    // Trigger change detection manually
-    this.cdr.detectChanges();
-  }
 }
