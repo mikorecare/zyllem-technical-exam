677ab6c35a8f8375154ccf166984a80620667795
SvenSven <mikorecare@gmail.com>
2023-11-30 20:10:12 +0800

    CHORE: Updated app module imports and declaration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { ZyllemApiService } from './app.service';
 import { ArticleModule } from './components/articles';
 import { HomeComponent } from './home.component';
-
+import { CommonModule } from '@angular/common';
+import { ViewDetailsComponent } from './components/view-details/view-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ViewDetailsComponent
   ],
   imports: [
+    CommonModule,
     AppRoutingModule,
     ArticleModule,
     FormsModule,
     BrowserModule,
-    
   ],
   providers: [ZyllemApiService],
   bootstrap: [AppComponent]
