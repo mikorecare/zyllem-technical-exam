8882a1d7af9ef8ffecb597441c7b73b2a587886c
SvenSven <mikorecare@gmail.com>
2023-11-30 14:15:24 +0800

    FEAT: Added parsing of query params

--- src/app/components/view-details/view-details.component.ts
+++ src/app/components/view-details/view-details.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Article } from 'src/app/model/article';
 
 @Component({
   selector: 'app-view-details',
   templateUrl: './view-details.component.html',
   styleUrls: ['./view-details.component.css']
 })
-export class ViewDetailsComponent {
+export class ViewDetailsComponent implements OnInit {
+  article!:Article
+  constructor(private route:ActivatedRoute){
 
+  }
+  ngOnInit(): void {
+   this.article = JSON.parse(decodeURIComponent(this.route.snapshot.queryParams.info))
+  }
 }
