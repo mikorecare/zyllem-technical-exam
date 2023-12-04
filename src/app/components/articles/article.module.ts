cef0adc5694e8bb31343ee1d2b1d7086c6d25109
SvenSven <mikorecare@gmail.com>
2023-12-04 09:42:49 +0800

    CHORE: removed unused imports

--- src/app/components/articles/article.module.ts
+++ src/app/components/articles/article.module.ts
@@ -3,16 +3,11 @@ import { NgModule } from "@angular/core";
 
 import { articleEntries } from "./article.entries";
 import { ArticleRendererComponent } from "./article.renderer.component";
-import { RouterModule } from "@angular/router";
-import { ViewDetailsComponent } from "../view-details/view-details.component";
+
 
 
 @NgModule({
     imports: [
-        CommonModule,
-        RouterModule.forChild([
-            { path: 'details', component: ViewDetailsComponent },
-          ]),
     ],
     declarations: [
         ...articleEntries,
